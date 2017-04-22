93f813ac6a830389bc478ce6fa01cfd8318ff5c4
Rongoel <goldmakrele@googlemail.com>
2017-04-22 21:34:35 +0200

    aufgabe 1

--- AUD/src/aufgabe1/Arrays.java
+++ AUD/src/aufgabe1/Arrays.java
@@ -1,7 +1,41 @@
 package aufgabe1;
 
 public class Arrays {
+	public static String[] expandArray(String[] arr){
+		
+		String[] newStringArray = new String[2*arr.length];
+		
+		for (int i = 0 ; i < arr.length; i++ ){
+			
+			newStringArray[i] = arr[i];
+			
+			
+		}
+		
+		return newStringArray;	
+		  
+	  }
+	
+	
+	public static String[] ShiftElements(String[] arr, int i){
+		
+		
+		String old = arr[i];
+	    String next = arr[i+1];
+		
+	    
+	    for(int j = i+1 ; j < arr.length; j++ ){
+	    	arr[j] = old;
+	    	old = next;
+	    	next = arr[j+1];
+	    }
+		
+	    return arr;
+		
+	
+	}
+	
 
-	String[] newStringArray = new String[] 
+	
 	
 }
